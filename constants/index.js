2c3f8a30d4a71d0deb28259ff651c783f7859643
snmsndhu <107799845+snmsndhu@users.noreply.github.com>
2022-11-06 08:15:14 -0500

    done

--- constants/index.js
+++ constants/index.js
@@ -1,5 +1,5 @@
 const WHITELIST_CONTRACT_ADDRESS = "0x92b78fF1B51b9F52Eb1575A3D5953E4772Facc52";
 
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://nft-collection-next-js.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
